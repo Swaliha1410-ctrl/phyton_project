84fb2cac6ae666ee2550b8c5cff13d9afbfeddd3
swaliha shaikh <salushaikh1410@gmail.com>
2025-12-03 23:41:17 +0530

    Update rock_paper_scissors.py

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -4,17 +4,20 @@ c='rps'
 count_comp=0
 count_user=0
 comp_ch=random.choice(c)
-for i in c:
+for i in range (3):
     try:
+       for i in c:
         user_ch=input('Choose stone,paper or scissors(r/p/s)').lower()
         print(f'your choice:-{emojis[user_ch]}')
-        print(f'computer choice:-{emojis[comp_ch]}')
+        print(f'computer choice:-{emojis[comp_ch]}') 
         if  comp_ch==user_ch:
-          pass
+            i+=1
         elif (comp_ch=='r' and user_ch=='p') or (comp_ch=='s' and user_ch=='r') or (comp_ch=='p' and user_ch=='s'):
-           count_user+=1
+            count_user+=1
+            i+=1
         else:
-           count_comp+=1
+            count_comp+=1
+            i+=1
     except:
         print('invalid choice,enter valid input')
 
@@ -24,4 +27,5 @@ elif count_comp<count_user:
     print(f'final count :- {count_user}" : " computer count:- {count_comp} you won ☺')
 else:
     print('it"s a tie!!')
+     
 
